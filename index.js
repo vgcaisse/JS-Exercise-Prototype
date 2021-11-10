5145cb09b0d7fb6fd872cc9e0a9f89f9eb9035af
Visnu <vgcaisse@gmail.com>
2021-11-10 17:03:22 -0500

    started working in the tasks, got 3 done

--- index.js
+++ index.js
@@ -39,15 +39,44 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(edible) {
+  if(this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
+}
 
+const kyler = new Person('Keyler', 25);
+const ben = new Person('Ben', 29);
 
+console.log(kyler.toString());
+console.log(ben.toString());
 
+ben.eat('Grilled-Chez');
+ben.eat('Grilled-Chez');
+ben.eat('Grilled-Chez');
+
+ben.eat('Grilled-Chez');
+ben.eat('Grilled-Chez');
+ben.eat('Grilled-Chez');
+ben.eat('Grilled-Chez');
+
+console.log(ben.stomach);
+ben.poop();
+console.log(ben.stomach);
 
 /*
   TASK 2
@@ -63,10 +92,30 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function(gallons) {
+  this.tank = this.tank + gallons;
+}
+
+Car.prototype.drive = function(distance) {
+  const driveMiles = this.tank * this.milesPerGallon;
+  if (driveMiles >= distance) {
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance / this.milesPerGallon);
+  } else {
+    this.odometer = this.odometer + this.milesPerGallon
+    this.tank = 0;
+    return `I ran out of fuel ${this.odometer} miles`;
+  }  
+}
+
+console.log(Car.drive(15));
 
 /*
   TASK 3
